ec08e090301dbadac4e1fcad53ad1268ac2cc0f2
李新 <13798516431@126.com>
2020-03-19 21:49:59 +0800

    swagger集成

--- swagger-integration-parent/swagger-integration/src/main/java/help/lixin/framework/swagger/config/SwaggerConfig.java
+++ swagger-integration-parent/swagger-integration/src/main/java/help/lixin/framework/swagger/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package help.lixin.framework.swagger.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @EnableConfigurationProperties(SwaggerProperties.class)
+@ConditionalOnProperty(prefix = "swagger", name = "enable", havingValue = "true")
 public class SwaggerConfig {
 
 	@Autowired
--- swagger-integration-parent/swagger-integration/src/main/java/help/lixin/framework/swagger/properties/SwaggerProperties.java
+++ swagger-integration-parent/swagger-integration/src/main/java/help/lixin/framework/swagger/properties/SwaggerProperties.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "swagger")
 public class SwaggerProperties {
+	// 是否启用
+	private boolean enable = true;
 	// 标题
 	private String title;
 	// 描述
@@ -16,6 +18,14 @@ public class SwaggerProperties {
 	private String contact;
 	// 版本
 	private String version;
+
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+
+	public boolean isEnable() {
+		return enable;
+	}
 
 	public String getTitle() {
 		return title;
