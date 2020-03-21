606af5c33b2e2a06271d29e87c854eeb5e795890
李新 <13798516431@126.com>
2020-03-21 15:37:39 +0800

    基础框架层

--- base-frame-parent/script/base-frame.sql
+++ base-frame-parent/script/base-frame.sql
@@ -19,6 +19,13 @@ CREATE TABLE t_role (
 ) COMMENT '角色信息表';
 
 
+-- 角色与权限关联表
+CREATE TABLE t_role_power (
+    role_power_id    BIGINT                       PRIMARY KEY COMMENT '主键',
+    role_id          BIGINT                       NOT NULL COMMENT '角色信息主键',
+    power_id         BIGINT                       NOT NULL COMMENT '权限ID'
+) COMMENT '角色与权限关联表';
+
 -- 权限表
 CREATE TABLE t_power (
     power_id     BIGINT        PRIMARY KEY     COMMENT '权限ID',
@@ -85,15 +92,45 @@ CREATE TABLE t_page_element (
 
 -- 功能操作(与页面元素有时确实是一对一,但是,建议拆出来细化)
 CREATE TABLE t_operation (
-	operation_id                 BIGINT         PRIMARY KEY  COMMENT '操作ID',
-	operation_parent_id          BIGINT         NOT NULL     COMMENT '操作父ID',
-	operation_name               VARCHAR(50)    NOT NULL     COMMENT '操作名称',
-	operation_code               VARCHAR(50)    NOT NULL     COMMENT '操作编码',
-	operation_url                VARCHAR(50)    NOT NULL     COMMENT '操作URL',
-	create_by                    VARCHAR(64)    DEFAULT ''   COMMENT '创建者',
-    create_time                  DATETIME                    COMMENT '创建时间',
-    update_by                    VARCHAR(64)    DEFAULT ''   COMMENT '更新者',
-    update_time                  DATETIME                    COMMENT '更新时间',
-    remark                       VARCHAR(500)   DEFAULT ''   COMMENT '备注'
+	operation_id                 BIGINT         PRIMARY KEY              COMMENT '操作ID',
+	operation_parent_id          BIGINT         DEFAULT -1  NOT NULL     COMMENT '操作父ID',
+	operation_name               VARCHAR(50)    NOT NULL                 COMMENT '操作名称',
+	operation_code               VARCHAR(50)    NOT NULL                 COMMENT '操作编码',
+	operation_url                VARCHAR(50)    NOT NULL                 COMMENT '操作URL',
+	create_by                    VARCHAR(64)    DEFAULT ''               COMMENT '创建者',
+    create_time                  DATETIME                                COMMENT '创建时间',
+    update_by                    VARCHAR(64)    DEFAULT ''               COMMENT '更新者',
+    update_time                  DATETIME                                COMMENT '更新时间',
+    remark                       VARCHAR(500)   DEFAULT ''               COMMENT '备注'
 ) COMMENT '功能操作';
 
+
+-- 378249758998925312
+-- 插入角色
+INSERT INTO t_role(role_id,role_name) VALUES(378249758998925332,'超级管理员');
+INSERT INTO t_role(role_id,role_name) VALUES(378249758998925333,'管理员');
+
+
+-- 插入用户与角色的关联
+INSERT INTO t_user_info_role(id,user_info_id,role_id) VALUES(378249758998925334,378249758998925312,378249758998925332);
+INSERT INTO t_user_info_role(id,user_info_id,role_id) VALUES(378249758998925335,378249758998925312,378249758998925333);
+
+
+-- 插入菜单
+INSERT INTO t_power(power_id,power_type) VALUES(378249758998925336,'menu');
+INSERT INTO t_power(power_id,power_type) VALUES(378249758998925337,'operation');
+
+-- 插入菜单
+INSERT INTO t_menu(menu_id,menu_name,menu_url,order_num,visible) VALUES(378249758998925338,'用户管理','/admin',1,1);
+-- 插入操作
+INSERT INTO t_operation(operation_id,operation_name,operation_code,operation_url) VALUES(378249758998925339,'测试','test','/hello');
+
+-- 插入菜单关联
+INSERT INTO t_power_menu(power_menu_id,power_id,menu_id) VALUES(378249758998925340,378249758998925336,378249758998925338);
+
+-- 插入菜单操作关联
+INSERT INTO t_power_operation(power_operation_id,power_id,operation_id) VALUES(378249758998925341,378249758998925337,378249758998925339);
+
+-- 插入角色所拥有的权限信息.
+INSERT INTO t_role_power(role_power_id,role_id,power_id) VALUES(378249758998925342,378249758998925332,378249758998925336);
+INSERT INTO t_role_power(role_power_id,role_id,power_id) VALUES(378249758998925343,378249758998925333,378249758998925337);
